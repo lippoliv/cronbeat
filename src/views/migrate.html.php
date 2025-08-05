88ae914f0f40d50032ebeced27387ef1e274a6cc
Oliver Lippert <oliver@allesit.de>
2025-08-05 11:15:11 +0200

    fix phpstan

--- src/views/migrate.html.php
+++ src/views/migrate.html.php
@@ -1,26 +1,35 @@
+<?php
+/**
+ * @var string|null $error Error message to display
+ * @var string|null $success Success message to display
+ * @var int $currentVersion Current database version
+ * @var int $expectedVersion Expected database version
+ * @var bool $needsMigration Whether migration is needed
+ */
+?>
 <h1>CronBeat Database Migration</h1>
 
-<?php if (isset($error) && $error) : ?>
+<?php if (isset($error) && $error !== '') : ?>
 <div class='error'><?= $error ?></div>
 <?php endif; ?>
 
-<?php if (isset($success) && $success) : ?>
+<?php if (isset($success) && $success !== '') : ?>
 <div class='success'><?= $success ?></div>
 <?php endif; ?>
 
 <div class="migration-info">
     <p>Current database version: <strong><?= $currentVersion ?></strong></p>
     <p>Expected database version: <strong><?= $expectedVersion ?></strong></p>
-    
+
     <?php if ($needsMigration) : ?>
         <div class="migration-needed">
             <p>Your database needs to be updated to the latest version.</p>
             <p>This will add new features and fix potential issues.</p>
-            
+
             <form method="post" action="/migrate">
                 <button type="submit" class="migrate-button">Update Database</button>
             </form>
-            
+
             <p class="migration-note">You can also run the following command from the terminal:</p>
             <pre class="command">php src/cli.php migrate</pre>
         </div>
